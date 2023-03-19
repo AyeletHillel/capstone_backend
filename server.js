9a3efbe96728f5bdccf1c010f53342604f7772ce
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-03-18 20:12:25 -0400

    fix3

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 // DEPENDENCIES
 /////////////////////////
 // get .env variables
-require("dotenv").config();
+// require("dotenv").config();
 // pull PORT from .env, give default value of 3000
-const { PORT = 3000, DATABASE_URL } = process.env;
+// const { PORT = 3000, DATABASE_URL } = process.env;
 // import express
 const express = require("express");
 // create application object
