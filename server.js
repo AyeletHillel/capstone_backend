83250e7bc6e89fa648e8d4a0fdd069afa1f0b305
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-03-30 14:59:30 -0400

    fix try2

--- server.js
+++ server.js
@@ -1,121 +1,82 @@
-/////////////////////////
-// DEPENDENCIES
-/////////////////////////
-// get .env variables
-require("dotenv").config()
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+
 const PORT = process.env.PORT || 4000
 const DATABASE_URL = process.env.DATABASE_URL
-// import express
-const express = require("express");
-// create application object
-const app = express();
-// import mongoose
-const mongoose = require("mongoose");
-// import middlware
-const cors = require("cors");
-const morgan = require("morgan");
 
+const app = express()
 
-///////////////////////////////
-// DATABASE CONNECTION
-////////////////////////////////
-// Establish Connection
 mongoose.connect(DATABASE_URL, {
-  useUnifiedTopology: true,
   useNewUrlParser: true,
-});
-// Connection Events
+  useUnifiedTopology: true,
+})
+
 mongoose.connection
-  .on("open", () => console.log("Your are connected to mongoose"))
-  .on("close", () => console.log("Your are disconnected from mongoose"))
-  .on("error", (error) => console.log(error));
-
-
-/////////////////////////
-// Models
-/////////////////////////
+  .on('open', () => console.log('You are connected to mongoose'))
+  .on('close', () => console.log('You are disconnected from mongoose'))
+  .on('error', (error) => console.log(error))
 
 const ResultsSchema = new mongoose.Schema({
-    input: String,
-    score: Number,
-    sentiment: String,
+  input: String,
+  score: Number,
+  sentiment: String,
 })
 
-const Results = mongoose.model("Results", ResultsSchema)
+const Results = mongoose.model('Results', ResultsSchema)
 
-///////////////////////////////
-// MiddleWare
-////////////////////////////////
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
 
-app.use(cors({ origin: '*' })); // to prevent cors errors, open access to all origins
-app.use(morgan("dev")); // logging
-app.use(express.json()); // parse json bodies
-
-
-/////////////////////////
-// Routes
-/////////////////////////
-
-app.get("/", (req, res) => {
-    res.json({
-        response: "Hello World"
-    })
+app.get('/', (req, res) => {
+  res.json({ response: 'Hello World' })
 })
 
-// Index
-app.get("/results", async (req, res) => {
-    try{
-        res.json(await Results.find({}))
-    } catch (error) {
-        res.status(400).json(error)
-    }
+app.get('/results', async (req, res) => {
+  try {
+    res.json(await Results.find({}))
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
-
-// Create
-app.post("/results", async (req, res) => {
-    try {
-        res.json(await Results.create(req.body));
-      } catch (error) {
-        //send error
-        res.status(400).json(error);
-      }
+app.post('/results', async (req, res) => {
+  try {
+    res.json(await Results.create(req.body))
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
-// Update 
-app.put("/results/:id", async (req, res) => {
-    try {
-        res.json(
-          await Results.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        );
-      } catch (error) {
-        //send error
-        res.status(400).json(error);
-      }
+app.put('/results/:id', async (req, res) => {
+  try {
+    res.json(
+      await Results.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    )
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
-// Delete
-app.delete("/results/:id", async (req, res) => {
-    try {
-        res.json(await Results.findByIdAndRemove(req.params.id));
-      } catch (error) {
-        //send error
-        res.status(400).json(error);
-      }
+app.delete('/results/:id', async (req, res) => {
+  try {
+    res.json(await Results.findByIdAndRemove(req.params.id))
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
-// Show 
-app.get("/results/:id", async (req, res) => {
-    try {
-        res.json(await Results.findById(req.params.id));
-      } catch (error) {
-        //send error
-        res.status(400).json(error);
-      }
+app.get('/results/:id', async (req, res) => {
+  try {
+    res.json(await Results.findById(req.params.id))
+  } catch (error) {
+    res.status(400).json(error)
+  }
 })
 
-/////////////////////////
-// Listener
-/////////////////////////
-
-app.listen(PORT, () => console.log(`Can you feel the love on port ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`Can you feel the love on port ${PORT}`)
+})
